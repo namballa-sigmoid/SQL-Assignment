96fa25f19b5a4c39b25bf6caa533a69519b7f6af
Namballa Mukesh <98635179+namballa-sigmoid@users.noreply.github.com>
2022-02-04 17:02:11 +0530

    Add files via upload

---
+++ sql_solutions.sql
@@ -0,0 +1,92 @@
+
+USE practice;
+
+SHOW TABLES;
+
+SELECT COUNT(*)
+FROM airbnb;
+
+ALTER TABLE airbnb
+ADD id INT NOT NULL AUTO_INCREMENT KEY;
+
+SELECT *
+FROM airbnb;
+
+/*Q1*/
+
+(SELECT date
+FROM airbnb
+ORDER BY date
+LIMIT 1)
+UNION
+(SELECT date
+FROM airbnb
+ORDER BY date DESC
+LIMIT 1);
+
+
+
+/*Q2*/
+
+SELECT listing_id, date, COUNT(*) AS cnt
+FROM airbnb
+GROUP BY listing_id, date
+HAVING COUNT(*)>1;
+
+SELECT *
+FROM airbnb AS a1
+INNER JOIN airbnb AS a2
+WHERE a1.id > a2.id
+AND a1.listing_id = a2.listing_id
+AND a1.date = a2.date;
+
+
+
+
+/*Q3*/
+
+CREATE TABLE airbnb_avail  (
+    listing_id INT,
+    available INT,
+    not_available INT,
+    availability DECIMAL(5,2)
+);
+
+INSERT INTO airbnb_avail
+SELECT listing_id, SUM(available = 't'), SUM(available = 'f'), SUM(available = 't')/COUNT(*)*100
+FROM airbnb
+GROUP BY listing_id;
+
+SELECT * FROM airbnb_avail;
+
+
+
+
+/*Q4*/
+
+SELECT COUNT(*) AS CNT
+FROM airbnb_avail
+WHERE availability > 50;
+
+
+SELECT COUNT(*) AS CNT
+FROM airbnb_avail
+WHERE availability > 75;
+
+
+
+
+/*Q5*/
+
+UPDATE airbnb
+SET price = NULL
+WHERE price = ' ';
+
+UPDATE airbnb
+SET price = TRIM(LEADING '$' FROM price);
+
+UPDATE airbnb
+SET price=CAST(price AS DECIMAL(6,2));
+
+/*Q6*/
+
