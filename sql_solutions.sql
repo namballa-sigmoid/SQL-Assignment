e0bf577995afcb4f07502bffb911d00b538d9479
Namballa Mukesh <98635179+namballa-sigmoid@users.noreply.github.com>
2022-02-04 18:11:55 +0530

    Update sql_solutions.sql

--- sql_solutions.sql
+++ sql_solutions.sql
@@ -3,7 +3,7 @@ USE practice;
 
 SHOW TABLES;
 
-SELECT COUNT(*)
+SELECT COUNT(*) 
 FROM airbnb;
 
 ALTER TABLE airbnb
@@ -36,9 +36,9 @@ HAVING COUNT(*)>1;
 SELECT *
 FROM airbnb AS a1
 INNER JOIN airbnb AS a2
-WHERE a1.id > a2.id
-AND a1.listing_id = a2.listing_id
-AND a1.date = a2.date;
+WHERE a1.id > a2.id 
+AND a1.listing_id = a2.listing_id 
+AND a1.date = a2.date; 
 
 
 
@@ -80,7 +80,7 @@ WHERE availability > 75;
 
 UPDATE airbnb
 SET price = NULL
-WHERE price = ' ';
+WHERE price = ' ' OR price = '' OR price = '$' OR price = '$0' OR price = '0';
 
 UPDATE airbnb
 SET price = TRIM(LEADING '$' FROM price);
@@ -88,5 +88,18 @@ SET price = TRIM(LEADING '$' FROM price);
 UPDATE airbnb
 SET price=CAST(price AS DECIMAL(6,2));
 
+ALTER TABLE airbnb
+MODIFY price DECIMAL(6,2);
+
+SELECT listing_id, MAX(price) AS Maximum, MIN(price) AS Minumum, AVG(price) as Average
+FROM airbnb
+GROUP BY listing_id;
+
 /*Q6*/
+
+SELECT listing_id, AVG(price)
+FROM airbnb
+GROUP BY listing_id
+HAVING AVG(price)>500;
+
 
